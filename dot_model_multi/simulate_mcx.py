9730b85c3fda33ceaa6c1ecfb32651412c87e9b1
Aysha munawwara   Rafik <amunawwara@gmail.com>
2025-05-14 00:16:17 +0530

    Delete dot_model_multi directory

--- dot_model_multi/simulate_mcx.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-def simulate_mcx(voxel_dim=(32, 32, 32), num_wavelengths=3):
-    # Placeholder MCX-style simulation: generate synthetic 3D absorption maps
-    return torch.rand((num_wavelengths, *voxel_dim))
--- dot_model_multi/visualize.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-def visualize_voxel_output(output_tensor):
-    output_np = output_tensor.squeeze().detach().cpu().numpy()
-    mid_slice = output_np[output_np.shape[0] // 2]
-    plt.imshow(mid_slice, cmap='hot')
-    plt.title("Mid-slice of Reconstructed Brain Activity")
-    plt.colorbar()
-    plt.show()
